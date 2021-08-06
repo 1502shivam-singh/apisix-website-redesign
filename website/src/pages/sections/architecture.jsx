f2f3dd059ad3a81765f57469b2f3e255feef9481
Shivam Singh <shivam152002@gmail.com>
2021-08-07 00:32:36 +0530

    UI: add subtle looping background animation to arch section and optimize it

--- website/src/pages/sections/architecture.jsx
+++ website/src/pages/sections/architecture.jsx
@@ -1,14 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HLDesign from "../../assets/images/infographs/Architecture_compress.svg";
 import "../../css/customTheme.css";
+import gsap from "gsap";
+import Pattern from "../../assets/images/PatternGrouped.svg";
 
 function Architecture(props) {
 
     const screenWidth = props.screenWidth;
 
+    useEffect(() => {
+        let strokePaths = []
+        for (let i=1; i<28; i++) {
+            strokePaths.push(".PatternGrouped_svg__p"+i);
+        } 
+
+        let tlStroke = gsap.timeline({
+            paused: true,
+            defaults: { // children inherit these defaults
+                ease: "power2.inOut",
+                yoyo: true,
+                repeat: -1,
+            },
+        });
+
+        tlStroke.fromTo(strokePaths, {
+            strokeDashoffset: 10000
+        }, {
+            strokeDashoffset: 0,
+            duration: 3,
+            stagger: "0.2",
+            ease: "power2.inOut",
+        });
+
+        let observer = new IntersectionObserver(onIntersection, {
+            root: null,
+            threshold: .6
+        })
+          
+        function onIntersection(entries, opts){
+            entries.forEach(entry =>  {
+                if (entry.isIntersecting) {
+                    tlStroke.paused(false);
+                    console.log(opts);
+                } else {
+                    tlStroke.paused(true);
+                }
+            }
+          );
+        }
+          
+        observer.observe( document.querySelector('.arch'));
+        
+        return () => {
+            tlStroke.pause(0).kill(true);
+            observer.disconnect();
+        }
+    }, []);
+
     return (
         <>
             <div className="arch">
+                <div style={{position: "absolute", display: "flex", justifyContent: "center", alignItems: "center", height: "120vh"}}>
+                    <Pattern style={{width: "100%", strokeWidth: "3", zIndex: "-10", opacity: "0.25", strokeDasharray: "10000"}}/>
+                </div>
                 <div>
                     <h3 className="arch-head">Building for large-scale, high value systems</h3>
                 </div>
